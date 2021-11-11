c587ee0e5696113e350baee7abaa8fe03951bc3c
AyeMyatMyatKhine <ayemyatmyatkhine90@gmail.com>
2021-11-11 15:14:18 +0630

    [admin_dashboard]create admin dashboard page

---
+++ Cinemax/resources/views/admin/admin_dashboard.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{'/admin/style.css'}}">
+    <title>Admin Dashboard</title>
+</head>
+<body>
+
+<div class="container">
+    <h1 class="heading">Admin Dashboard</h1>
+    <div class="manage-sec">
+        <a href="#">
+            <div class="manage-movie">
+                <h3>Manage<br>Movies</h3>
+            </div>
+        </a>
+        <a href="#">
+            <div class="manage-booking ">
+                <h3>Manage <br>Bookings</h3>
+            </div>
+        </a>
+        <a href="#">
+            <div class="statistics">
+                <h3>Statistics</h3>
+            </div>
+        </a>
+    </div>
+</div>
+
+<div class="theater-update">
+    <div class="ttl">
+        <h1 class="heading">Update Theaters</h1>
+    </div>
+    <div class="container">
+        <a href="#">
+            <div class="manage-theater">
+                <h3>Manage <br>Theaters</h3>
+            </div>
+        </a>
+    </div>
+</div>
+</body>
+</html>
+
