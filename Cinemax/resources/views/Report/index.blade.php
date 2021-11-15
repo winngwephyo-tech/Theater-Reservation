a615164e6505c1994bf165edc0b34cd1053156dd
naylin2 <nayylinhtoo@gmail.com>
2021-11-15 14:59:51 +0630

    added css

--- Cinemax/resources/views/Report/index.blade.php
+++ Cinemax/resources/views/Report/index.blade.php
@@ -1,27 +1,43 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/reset.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/report/style.css') }}">
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 </head>
 <body>
-    
-<a class="btn btn-success ml-3" href="{{ url('/export_reports') }}"> Export</a>
-<a class="btn btn-success ml-3" href="{{ url('/delete_and_export_reports') }}"> Delete and Export</a>
-<table class="table table-bordered mt-5">
-        <tr>
-            <th>No</th>
-            <th>Movie Title</th>
-            <th>Income</th>
-            <th>Rating</th>
-        </tr>
-        @foreach ($reports as $report)
-        <tr>
-            <td>{{ $report->id }}</td>
-            <td>{{ $report->title }}</td>
-            <td>{{ $report->income }}</td>
-            <td>{{ $report->rating }}</td>
-        </tr>
-        @endforeach
+<div class="wrapper mt-20">
+
+    <div class="clearfix">
+        <div class="left">
+            <h2>Statistics</h2>
+        </div>
+        <div class="right">
+            <a class="button" href="{{ URL::previous() }}"> Back</a>
+        </div>
+    </div>
+    <div class="mt-20">
+    <a class="button" href="{{ url('/export_reports') }}"> Export</a>
+    <a class="button ml-10 button2" href="{{ url('/delete_and_export_reports') }}"> Delete & Export</a>
+    </div>
+    <table class="report mt-20">
+            <tr>
+                <th>No</th>
+                <th>Movie Title</th>
+                <th>Income</th>
+                <th>Rating</th>
+            </tr>
+            @foreach ($reports as $report)
+            <tr>
+                <td>{{ $report->id }}</td>
+                <td>{{ $report->title }}</td>
+                <td>{{ $report->income }}</td>
+                <td>{{ $report->rating }}</td>
+            </tr>
+            @endforeach
     </table>
+
+</div>
+
 </body>
 </html>
