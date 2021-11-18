7cd0ceb640d3cfb18b7096b9e3f7b0a73abdb735
winngwephyo-tech <ngwephyowin@gmail.com>
2021-11-18 21:17:31 +0630

    [develop]add css for movie_list

--- Cinemax/resources/views/movie/manage_movie.blade.php
+++ Cinemax/resources/views/movie/manage_movie.blade.php
@@ -11,44 +11,48 @@
         <h1 class="heading">Manage Movies</h1>
         <a href="#" class="button">Back</a>
     </div>
-    <div class="show-list">
+    <ul class="recent-list clearfix">
+
         @for($i=1 ; $i<=$no_of_theater ; $i++) @foreach ($showingMovie_result as $data) @if ($i==$data->theater_id)
-            <div class="img">
+            <li>
                 <a href="{{  route('movie.edit',$data->id) }}"><img src="/image/{{$data->poster}}" alt="Showing Movie Poster"></a>
                 <p>{{ $data->title }}<br> {{ $data->duration }}mins</p>
-            </div>
+            </li>
+
             @endif
-            </div>
             @endforeach
-
-    @endfor
-    <a href="{{  route('movie.create') }}">
-        <div class="add-movie">
-            <i class="fas fa-plus"></i>
-        </div>
-    </a>
+            @endfor
+            <li>
+                <a href="{{  route('movie.create') }}">
+                    <div class="add-movie">
+                        <i class="fas fa-plus"></i>
+                    </div>
+                </a>
+            </li>
+    </ul>
 </div>
-</div>
-
 <!-- manage upcomming -->
 <div class="wrapper">
     <div class="upcoming-list">
         <div class="ttl">
             <h1 class="heading">Upcoming Movies</h1>
         </div>
-        <div class="show-list">
+        <ul class="recent-list clearfix">
             @foreach ($upcomingMovie_result as $item)
-            <div class="img">
-                <a href="{{route('upmovie.edit',$item->id) }}"><img src="/upimage/{{$item->poster}}" alt="Showing Movie Poster"></a>
-                <p>{{ $item->title }} <br> {{ $item->duration }}min</p>
-            </div>
+            <li>
+                <a href="{{  route('movie.edit',$data->id) }}"><img src="/image/{{$data->poster}}" alt="Showing Movie Poster"></a>
+                <p>{{ $data->title }}<br> {{ $data->duration }}mins</p>
+
+            </li>
             @endforeach
-        </div>
-        <a href="{{route('upmovie.create') }}">
-            <div class="add-movie">
-                <i class="fas fa-plus"></i>
-            </div>
-        </a>
+            <li>
+                <a href="{{route('upmovie.create') }}">
+                    <div class="add-movie">
+                        <i class="fas fa-plus"></i>
+                    </div>
+                </a>
+            </li>
+        </ul>
     </div>
 </div>
 @endsection
--- Cinemax/resources/views/movie/movie_list.blade.php
+++ Cinemax/resources/views/movie/movie_list.blade.php
@@ -5,37 +5,33 @@
 @endsection
 
 @section('content')
-    <div class="wrapper">
-        <h1 class="heading">Now Showing</h1>
-        <div class="show-list">
-            @for($i=1 ; $i<= $no_of_theater ; $i++)
-            <div class="img">
-               @foreach ($showingMovie_result as $data)
-                    @if($i == $data->theater_id)
-
-                    <a href="{{route('description_movie',$data->id) }}"><img src="/image/{{$data->poster}}" alt="Movie Poster"></a>
-                           <p>{{ $data->title }}<br> {{ $data->duration }}mins</p>
-
-                    @endif
-                @endforeach
-            </div>
+<div class="wrapper">
+    <h1 class="heading">Now Showing</h1>
+    <ul class="recent-list clearfix">
+        @for($i=1 ; $i<= $no_of_theater ; $i++) @foreach ($showingMovie_result as $data) @if($i==$data->theater_id)
+            <li> <a href="{{route('description_movie',$data->id) }}"><img src="/image/{{$data->poster}}" alt="Movie Poster"></a>
+                <p>{{ $data->title }}<br> {{ $data->duration }}mins</p>
+            </li>
+            @endif
+            @endforeach
             @endfor
-        </div>
-    </div>
-    <!-- UPCOMMING -->
-    <div class="upcoming-list">
+    </ul>
+</div>
+<!-- UPCOMMING -->
+<div class="upcoming-list">
     <div class="ttl">
         <h1 class="heading">Upcoming Movies</h1>
     </div>
     <div class="wrapper">
-        <div class="show-list">
-                  <div class="img">
-                @foreach ($upcomingMovie_result as $item)
-                  <img src="/upimage/{{$item->poster}}" alt="Upcoming Movie Poster"> <p>{{ $item->title }} <br> {{ $item->duration }}min</p>
-                @endforeach
-            </div>
-        </div>
+        <ul class="recent-list clearfix">
+            @foreach ($upcomingMovie_result as $item)
+            <li> <img src="/upimage/{{$item->poster}}" alt="Upcoming Movie Poster">
+                <p>{{ $item->title }} <br> {{ $item->duration }}min</p>
+            </li>
+            @endforeach
+        </ul>
     </div>
+</div>
 </div>
 description_movie
 
