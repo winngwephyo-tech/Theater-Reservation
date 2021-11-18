be4acfe569736192534399ef55b34149326d1d9b
AyeMyatMyatKhine <ayemyatmyatkhine90@gmail.com>
2021-11-18 15:54:37 +0630

    modified css

--- Cinemax/resources/views/movie/movie_description.blade.php
+++ Cinemax/resources/views/movie/movie_description.blade.php
@@ -7,20 +7,19 @@
 @section('content')
     <div class="wrapper clearfix">
        <div class="movie-poster">
-          <img src="/image/{{ $movie->poster }}" alt="">
+          <img src="/image/{{ $movie->poster }}" alt="Movie poster">
        </div>
        <div class="movie-description">
            <div class="title clearfix">
                <div class="movie-title">
-                   <h2>{{ $movie->title }} <br> <span>{{ $movie->duration }}mins</span></h2>
+                   <h1>{{ $movie->title }} </h1>
+                   <span>{{ $movie->duration }}mins</span>
                </div>
                <div class="btn">
                     <a href="" class="trailer-btn button">Trailer</a>
-                    <div>
-                        @foreach ($showtime as $item)
-                            <a href="#" class="button">{{ $item -> showtime}}</a>
-                        @endforeach
-                    </div>
+                    @foreach ($showtime as $item)
+                        <a href="#" class="button book-btn">{{ $item -> showtime}}</a>
+                    @endforeach
                </div>
            </div>
            <div class="movie-details">
