0e9320ba984d6591f2aaf8c72fb791306f734bfb
naylin2 <nayylinhtoo@gmail.com>
2021-11-25 08:23:40 +0630

    bug fix

--- Cinemax/resources/views/layouts/app.blade.php
+++ Cinemax/resources/views/layouts/app.blade.php
@@ -7,10 +7,10 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   @yield('style')
-  <link href="{{ asset('css/common.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('css/common.css') }}" rel="stylesheet" type="text/css">
   @yield('script')
   <!-- Script -->
-  <script src="{{ asset('js/common/nav.js') }}"></script>
+    <script src="{{ asset('js/common/nav.js') }}"></script>
 
   <title>Cinemax</title>
 </head>
