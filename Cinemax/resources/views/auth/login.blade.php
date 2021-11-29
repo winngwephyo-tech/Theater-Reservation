250f918a0942fb93765f241cd4258c46be6a93fb
may <maythinkhaingminoz@gmail.com>
2021-11-29 22:51:23 +0630

    [feature/style_fix]design modify for user profile

--- Cinemax/resources/views/auth/login.blade.php
+++ Cinemax/resources/views/auth/login.blade.php
@@ -1,56 +1,55 @@
-
 <link rel="stylesheet" type="text/css" href="{{ asset('css/reset.css') }}">
 <link rel="stylesheet" type="text/css" href="{{ asset('css/login/style.css') }}">
 
 <div class="wrapper">
-            <div class="card">
-                <h2>Login</h2>
+    <div class="card">
+        <h2>Login</h2>
+        <div>
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+
                 <div>
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
 
-                        <div>
+                    <div>
+                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
-                            <div>
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div>
-
-                            <div>
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                <spanrole="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div>
-                            <div>
-                                <button type="submit" class="button">
-                                    {{ __('Login') }}
-                                </button>
-
-                                <p class="f-12 p-20"><a href="{{route('register')}}">Create New Account</a></p>
-
-                                @if (Route::has('password.request'))
-                                <a href="{{ route('password.request') }}" class="f-12">
-                                    {{ __('Forgot Your Password?') }}
-                                </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+                        @error('email')
+                        <span class="invalid-input" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
                 </div>
-            </div>
+
+                <div>
+
+                    <div>
+                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                        @error('password')
+                        <span class="invalid-input" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
+                </div>
+
+                <div>
+                    <div>
+                        <button type="submit" class="button">
+                            {{ __('Login') }}
+                        </button>
+
+                        <p class="f-12 p-20"><a href="{{route('register')}}">Create New Account</a></p>
+
+                        @if (Route::has('password.request'))
+                        <a href="{{ route('password.request') }}" class="f-12">
+                            {{ __('Forgot Your Password?') }}
+                        </a>
+                        @endif
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
 </div>
